02be991d8e8d10791d55ba4a0af493c401338f18
Murilo-Hasse <murilohassemh@gmail.com>
2024-05-29 21:56:44 -0300

    Finalização dos Scripts de Criação das Tabelas atuais - 29/05/2024

--- models.py
+++ models.py
@@ -1,8 +1,6 @@
 from pony.orm import Database, Required, Optional, Set, PrimaryKey
-from pathlib import Path
-
-CURRENT_PATH = Path(__file__).parent
-DATABASE_PATH = CURRENT_PATH / 'db.sqlite3'
+from decimal import Decimal
+from datetime import datetime
 
 db = Database()
 
@@ -10,25 +8,72 @@ db = Database()
 class Funcao(db.Entity):
     codigo = PrimaryKey(int, auto=True)
     descricao = Required(str, max_len=63)
-    
-    
+    pessoas = Set('Pessoa')
+
+
 class Endereco(db.Entity):
     codigo = PrimaryKey(int, auto=True)
     logradouro = Required(str, max_len=63)
     numero = Required(int)
     cep = Required(str, max_len=8)
     bairro = Required(str, max_len=63)
-    
-    
+    pessoas = Set('Pessoa')
+
+
 class Pessoa(db.Entity):
     codigo = PrimaryKey(int, auto=True)
     nome = Required(str, max_len=45)
     senha = Required(str, max_len=50)
     funcao = Required(Funcao)
     endereco = Required(Endereco)
-    
-    
+    contatos = Set('PessoaContato')
+    produtos = Set('Produto')
+    vendas = Set('Venda')
 
-if __name__ == '__main__':
-    db.bind(provider='sqlite', filename=DATABASE_PATH)
-    db.generate_mapping(create_tables=True)
+
+class TipoContato(db.Entity):
+    codigo = PrimaryKey(int, auto=True)
+    descricao = Required(str, max_len=63)
+    contatos = Set('PessoaContato')
+
+
+class PessoaContato(db.Entity):
+    codigo = PrimaryKey(int, auto=True)
+    pessoa = Required(Pessoa)
+    tipo = Required(TipoContato)
+
+
+class FormaDePagamento(db.Entity):
+    codigo = PrimaryKey(int, auto=True)
+    descricao = Required(str, max_len=63)
+    vendas = Set('Venda')
+
+
+class Produto(db.Entity):
+    codigo = PrimaryKey(int, auto=True)
+    descricao = Required(str, max_len=255)
+    valor = Required(Decimal, precision=10, scale=2)
+    quantidade = Required(int)
+    fornecedor = Required(Pessoa)
+    itens = Set('Item')
+
+
+class Venda(db.Entity):
+    codigo = PrimaryKey(int, auto=True)
+    horario = Required(datetime, sql_default='CURRENT_TIMESTAMP')
+    valor_total = Optional(Decimal, precision=10, scale=2)
+    funcionario = Required(Pessoa)
+    forma_pagamento = Required(FormaDePagamento)
+    itens = Set('Item')
+
+
+class Item(db.Entity):
+    codigo = PrimaryKey(int, auto=True)
+    quantidade = Required(int)
+    valor_parcial = Required(Decimal, precision=10, scale=2)
+    produto = Required(Produto)
+    venda = Required(Venda)
+
+
+db.bind(provider='sqlite', filename='db.sqlite3')
+db.generate_mapping(create_tables=True)

7d51ff59fae8976c45c641230dfe35e7da7facf2
Matheus-Merlos <matheusazrmerlos@gmail.com>
2024-05-31 13:36:21 -0300

    Criei o decodificador de b64 para imagem, e criei a função de decodificar e dar upload no dropbox

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import dropbox
+import base64
 import os
 
 def upload_image_to_dropbox(img_path: str, img_name: str) -> str:
@@ -21,6 +22,24 @@ def upload_image_to_dropbox(img_path: str, img_name: str) -> str:
     #transforma o link do dropbox em um link de imagem em si
     return shared_link_metadata.url.replace("www.dropbox.com", "dl.dropboxusercontent.com")
 
+
+def decode_b64_to_img(b64_str: str, nome_arquivo: str, extensao_img: str) -> None:
+    image_bytes = base64.b64decode(b64_str)
+    
+    with open(f'img/{nome_arquivo}.{extensao_img}', 'wb') as file:
+        file.write(image_bytes)
+        
+
+def decode_and_upload_to_dropbox(b64_img_str: str, img_name: str, img_extension: str) -> str:
+    decode_b64_to_img(b64_img_str, img_name, img_extension)
+    url = upload_image_to_dropbox(f'img/{img_name}.{img_extension}', f'{img_name}.{img_extension}')
+    
+    return url
+
 if __name__ == "__main__":
-    print(f'link da imagem = {upload_image_to_dropbox("img/ryzen_2.jpg", "ryzen_3.jpg")}')
+    with open('img/img_b64.txt') as file:
+        b64_img = file.read()
+    
+    
+    print(decode_and_upload_to_dropbox(b64_img, 'ryzen', 'jpeg'))
 

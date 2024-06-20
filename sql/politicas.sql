1f176dc417d923d895da3137e0e8ca066895a2b1
Murilo Hasse <127515402+Murilo-Hasse@users.noreply.github.com>
2024-06-20 14:40:04 -0300

    Update politicas.sql aplicação da politica de acesso próprio para a tabela venda para usuario normais

--- sql/politicas.sql
+++ sql/politicas.sql
@@ -1,6 +1,6 @@
-
 GRANT SELECT ON pessoa TO grupo_usuario;
 
+--Habilita o controle de acesso na tabela
 ALTER TABLE pessoa ENABLE ROW LEVEL SECURITY;
 
 --Politica do acesso próprio, usuarios convencionais (usuários da aplicação), só terão acesso aos seus próprios dados, a forma de garantir isso tornando a visibilidade dos dados das tabelas apenas aqueles que o documento for igual 
@@ -9,5 +9,21 @@ ON pessoa
 FOR SELECT
 TO grupo_usuario
 USING (documento  = current_user);
+
+ALTER TABLE pessoa FORCE ROW LEVEL SECURITY;
+
+GRANT SELECT ON venda TO grupo_usuario;
+
+--Habilita o controle de acesso na tabela
+ALTER TABLE venda ENABLE ROW LEVEL SECURITY;
+ 
+CREATE POLICY acesso_proprio_venda
+ON venda
+FOR SELECT
+TO grupo_usuario
+USING ((SELECT P.documento 
+			FROM venda as V
+			INNER JOIN pessoa as P 
+			ON v.codigo_usuario= p.codigo)  = current_user);
 
 ALTER TABLE pessoa FORCE ROW LEVEL SECURITY;

c75f227be191c808576fff010d6aa95c9dd8dd77
Murilo Hasse <127515402+Murilo-Hasse@users.noreply.github.com>
2024-06-20 18:39:28 -0300

    Update groups.sql

--- sql/groups.sql
+++ sql/groups.sql
@@ -4,14 +4,16 @@ CREATE ROLE padrao with password '1234';
 -- 2. Concessão de permissões para criar outros usuários
 ALTER USER padrao WITH CREATEROLE;
 
--- 3. Concessão de permissões para inserir dados em uma tabela específica
-GRANT INSERT ON minha_tabela TO padrao;
+
 
 -- Revogar execução de todas as funções no esquema public (ou outro esquema específico)
 REVOKE ALL ON ALL FUNCTIONS IN SCHEMA public FROM padrao;
 
 -- Revogar qualquer outra permissão que não seja necessária
 REVOKE ALL ON SCHEMA public FROM padrao;
+
+-- 3. Concessão de permissões para inserir dados em uma tabela específica
+GRANT INSERT ON minha_tabela TO padrao;
 
 --Garante que usuario padrão possa executar a função de registro
 GRANT EXECUTE ON FUNCTION user_table_to_user TO padrao;

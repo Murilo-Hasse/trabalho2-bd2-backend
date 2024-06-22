3f3e43a5c37401306da7ec52b1323932027cabc0
Murilo Hasse <127515402+Murilo-Hasse@users.noreply.github.com>
2024-06-22 13:59:34 -0300

    Update groups.sql

--- sql/groups.sql
+++ sql/groups.sql
@@ -2,10 +2,10 @@
 CREATE ROLE grupo_usuario;
 
 --Dar permissão a consultar na tabela produto. Restrição pela politica do acesso prórpio
-GRANT SELECT ON TABLE produto TO grupo_usuario;
+GRANT SELECT ON TABLE pessoa TO grupo_usuario;
 
 --Dar permissão a consultar na tabela venda. Restrição pela politica do acesso prórpio
-GRANT SELECT ON venda TO grupo_usuario;
+GRANT SELECT ON TABLE venda TO grupo_usuario;
 
 --Dar permissão a consultar na tabela produto. Acesso total
-GRANT SELECT ON produto TO grupo_usuario;
+GRANT SELECT ON TABLE produto TO grupo_usuario;

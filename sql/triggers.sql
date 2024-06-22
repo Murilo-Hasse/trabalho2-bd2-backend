c62130ebc64743246ce8c1697814aa2f96094927
Murilo Hasse <127515402+Murilo-Hasse@users.noreply.github.com>
2024-06-22 14:32:48 -0300

    Update triggers.sql Adição dos usuarios no grupo

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -14,6 +14,7 @@ BEGIN
     passwd := last_user.senha;
 
     EXECUTE 'CREATE USER ' || email || ' WITH PASSWORD ' || quote_literal(passwd);
+	EXECUTE 'ALTER GROUP grupo_usuario ADD USER' || email;
 
     RETURN NEW;
 END;
@@ -23,5 +24,3 @@ CREATE OR REPLACE TRIGGER create_user
 AFTER INSERT ON pessoa
 FOR EACH ROW
 EXECUTE FUNCTION user_table_to_user();
-
-
